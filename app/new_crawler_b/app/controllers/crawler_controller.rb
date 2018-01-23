2e2c2c7f928f866c90fd243a5a44a87be349d8d2
Victor Guerra <victorbcguerra@gmail.com>
2018-01-23 09:47:05 -0200

    Corrigindo a função para buscar os e-mails na url

--- app/new_crawler_b/app/controllers/crawler_controller.rb
+++ app/new_crawler_b/app/controllers/crawler_controller.rb
@@ -22,11 +22,34 @@ class CrawlerController < ApplicationController
       end
     end
     
-    CrawlerWorker.perform_async(urls_to_add)
+    include(urls_to_add)
+    # CrawlerWorker.perform_async(urls_to_add)
     redirect_to root_path
   end
   
   #Função que na urls buscada, encontra emails, caso o email for repetido, ele não é inserido no BD.
+  def include(urls_crawled)
+    urls_crawled.each do |c|
+      if !c.nil?
+        if c.include? "mailto:"
+          url = Email.new
+          email = c[7..c.length]
+          url.email = email
+          count = Email.where(:email => email).count
 
+          if count == 1
+            url.email = nil
+          end
+        else
+          url = Email.new
+          url.email = nil
+        end
+
+        if url.valid?
+          url.save
+        end
+      end
+    end
+  end
 
 end
